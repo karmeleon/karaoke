0ac86350f615ac4d7e3665281430e056a6cbacd6
Shawn Walton <sh@wn.zone>
2019-05-12 13:38:05 -0700

    add basic landing page

--- frontend/src/components/App/App.tsx
+++ frontend/src/components/App/App.tsx
@@ -1,7 +1,10 @@
+import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
-import React, { FC } from 'react';
-import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
+import Grid from '@material-ui/core/Grid';
+import React, { FC, useState } from 'react';
+import { BrowserRouter as Router, Link, Route, Redirect } from 'react-router-dom';
+
 import KaraokeBase from '../KaraokeBase';
 import RemoteBase from '../RemoteBase';
 
@@ -12,10 +15,45 @@ const theme = createMuiTheme({
 	typography: { useNextVariants: true },
 });
 
-const App: FC<{}> = () => <MuiThemeProvider theme={theme}><CssBaseline />
+const AppSelector: FC<{}> = () => {
+	const [redirectUrl, setRedirectUrl] = useState<string | null>(null);
+
+	if (redirectUrl !== null) {
+		return <Redirect push to={redirectUrl} />
+	}
+
+	return (
+		<Grid container direction="column" justify="center" alignItems="center">
+			<Grid item xs={3}>
+				<Button 
+					onClick={() => setRedirectUrl('/karaoke')}
+					color="primary"
+					variant="contained"
+					fullWidth
+				>
+					Karaoke
+				</Button>
+			</Grid>
+			<Grid item xs={3}>
+				<Button
+					onClick={() => setRedirectUrl('/remote')}
+					color="primary"
+					variant="contained"
+					fullWidth
+				>
+					Remote
+				</Button>
+			</Grid>
+		</Grid>
+	);
+};
+
+const App: FC<{}> = () => 
+<MuiThemeProvider theme={theme}><CssBaseline />
 	<Router>
 		<Route path="/remote" component={RemoteBase} />
 		<Route path="/karaoke" component={KaraokeBase} />
+		<Route path="/" exact component={AppSelector} />
 	</Router>
 </MuiThemeProvider>;
 
